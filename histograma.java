79c35efc29b83ed966801c726ad2aee6aaa5c5fe
stivenrodriguezm <stivenrodriguez1996m@gmail.com>
2023-09-09 00:21:54 -0500

    AdivinaUnNumero  funcionando correctamente

---
+++ histograma.java
@@ -0,0 +1,33 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+    public class histograma {
+        public static void main(String[] args) {
+            Scanner lector = new Scanner(System.in);
+            int valor = 1;
+            ArrayList<String> positivos = new ArrayList<String>();
+            ArrayList<String> negativos = new ArrayList<String>();
+
+            while (valor != 0) {
+                System.out.println("Ingresa un valor entero:");
+                valor = lector.nextInt();
+
+                if (valor > 0) {
+                    positivos.add("*");
+                } else if (valor < 0) {
+                    negativos.add("*");
+                }
+            }
+
+            System.out.print("Positivos: ");
+            for (String i : positivos) {
+                System.out.print(i + " ");
+            }
+
+            System.out.print("\nNegativos: ");
+            for (String i : negativos) {
+                System.out.print(i + " ");
+            }
+        }
+    }
